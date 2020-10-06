26e3015709812f3ab439f2ea99bede3f73c9fa9b
RAMAKRISHNA DEVARAKONDA <ramakrishnadevarakonda@RAMAKRISHNAs-iMac.local>
2020-10-06 16:12:39 -0500

    PyPoll script is ready

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,112 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[85]:
 
 
-import csv
-path = '/Users/ramakrishnadevarakonda/Desktop/python-challenge/BootCamp-HomeWork-python-challenge/Resources/budget_data.csv'
+from pathlib import Path
+
+input_file = Path("BootCamp-HomeWork-python-challenge",'PyBank','Resources','budget_data.csv')
 total_number_of_months=[]
 profit_loss=[]
 average_change=[]
-with open (path, 'r') as csv_file:
+
+with open (input_file, 'r') as csv_file:
     csv_reader = csv.reader(csv_file,delimiter=',')
     next(csv_reader)
     data=[row for row in csv_reader]
     for row in data:
         total_number_of_months.append(row[0])
         profit_loss.append(int(row[1]))
+    
+    for i in range(len(profit_loss)-1):
+        average_change.append(profit_loss[i+1] - profit_loss[i])
 
-for i in range(len(total_profit)-1):
-    monthly_profit_change.append(total_profit[i+1] - total_profit[i])
+max_increase_month = average_change.index(max(average_change)) + 1
+min_increase_month = average_change.index(min(average_change)) + 1
 
-max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-min_increase_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
-print("Financial Analysis")
-print('-' * len("Financial Analysis"))
+output_file = Path("BootCamp-HomeWork-python-challenge",'PyBank','Analysis','Financial_Analysis.txt')
+
+with open(output_file, 'w') as file:
+    file.write('Financial Analysis')
+    file.write('\n')
+    file.write('-------------------')
+    file.write('\n')
+    file.write('Total Months: {}'.format(len(total_number_of_months)))
+    file.write('\n')
+    file.write('Total:'+'$'+str(sum(profit_loss)))
+    file.write('\n')
+    file.write(f"Average Change:${round(sum(average_change)/len(average_change),2)}")
+    file.write('\n')
+    file.write('Greatest Increase in Profits:{} ({}{})'.format(total_number_of_months[max_increase_month],'$',max(average_change)))
+    file.write('\n')
+    file.write('Greatest Decrease in Profits:{} ({}{})'.format(total_number_of_months[min_increase_month],'$',min(average_change)))
+
+# Print statements
+print("Financial Analysis\n",'-'*len("Financial Analysis"))
 print('Total Months: {}'.format(len(total_number_of_months)))
 print('Total:'+'$'+str(sum(profit_loss)))
-print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print('Greatest Increase in Profits:{} ({}{})'.format(total_number_of_months[max_increase_month],'$',max(monthly_profit_change)))
-print('Greatest Decrease in Profits:{} ({}{})'.format(total_number_of_months[min_increase_month],'$',min(monthly_profit_change)))
+print(f"Average Change: {round(sum(average_change)/len(average_change),2)}")
+print('Greatest Increase in Profits:{} ({}{})'.format(total_number_of_months[max_increase_month],'$',max(average_change)))
+print('Greatest Decrease in Profits:{} ({}{})'.format(total_number_of_months[min_increase_month],'$',min(average_change)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
---
+++ PyPoll/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 11:34:33 2020
+
+@author: ramakrishnadevarakonda
+"""
+
+import csv
+from pathlib import Path
+
+input_file = Path("BootCamp-HomeWork-python-challenge",'PyPoll','Resources','election_data.csv')
+output_file = Path("BootCamp-HomeWork-python-challenge",'PyPoll','Analysis','Election_Result.txt')
+total_no_of_votes=[];list_of_candidates=[];candidate_votes=[];names_and_votes=[]
+with open(output_file, 'w') as file:
+    with open(input_file,'r') as csv_file:
+        csv_reader = csv.reader(csv_file,delimiter=',')
+        next(csv_reader)
+        data=[row for row in csv_reader]
+    # Total number of votes
+        [total_no_of_votes.append(row[0]) for row in data]
+    # Capture all the candidates
+        [list_of_candidates.append(row[2]) for row in data if row[2] not in list_of_candidates]
+        print('Election Results')
+        print('----------------')
+        print('Total Votes:' +str(len(total_no_of_votes)))
+        print('--------------------')
+        file.write('Election Results')
+        file.write('\n')
+        file.write('-------------------')
+        file.write('\n')
+        file.write('Total Votes: {}'.format(len(total_no_of_votes)))
+        file.write('\n')
+        file.write('-------------------')
+    #Calculate all the votes for each candidate
+        for candidate in list_of_candidates:
+            for row in data:
+                if row[2]==candidate:
+                    candidate_votes.append(row[0])
+            
+            print(f"{candidate}:{(len(candidate_votes) / len(total_no_of_votes) * 100):.3f}%({len(candidate_votes)})")
+            file.write(f"{candidate}:{(len(candidate_votes) / len(total_no_of_votes) * 100):.3f}%({len(candidate_votes)})")
+            file.write('\n')
+    #create a list with all the candidate votes
+            names_and_votes.append((len(candidate_votes)))
+            candidate_votes=[]
+    # create a dictionary using 2 lists
+        d = dict(zip(list_of_candidates,names_and_votes))
+        
+        winner=max(d,key=d.get)
+        print('-----------------------')
+        file.write('-------------------')
+        file.write('\n')
+        print('Winner:' + winner)
+        print('-----------------')
+        file.write('Winner:' +winner)
+        file.write('\n')
+        file.write('-----------------')
+    
