57719297ec3605db373ce8c7c8ffa09f689161de
RAMAKRISHNA DEVARAKONDA <ramakrishnadevarakonda@RAMAKRISHNAs-iMac.local>
2020-10-07 14:49:50 -0500

    PyParagraph_1 script is completed

---
+++ PyParagraph/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 12:38:54 2020
+
+@author: ramakrishnadevarakonda
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 16:22:35 2020
+
+@author: ramakrishnadevarakonda
+"""
+
+from pathlib import Path
+word_count=[]
+input_file = Path("BootCamp-HomeWork-python-challenge",'PyParagraph','Resources','paragraph_1.txt')
+with open(input_file,'r') as txt_file:
+   txt_reader = txt_file.read()
+
+    
+   words = len([word for word in txt_reader.split(" ")])
+   sentences = len([word for word in txt_reader if word in(".",'?','!')])
+   print('Paragraph Analysis')
+   print('-------------------')
+   print('Approximate Word Count:', words)
+   print(f'Average Letter Count: {len([c for c in txt_reader if c.isalpha()])/words:.2f}')
+   print(f'Average Sentence Length: {(words/sentences):.2f}')
+   
+
+   
+output_file = Path("BootCamp-HomeWork-python-challenge",'PyParagraph','Analysis','Paragraph Analysis.txt')
+with open(output_file,'w') as out_file:
+    out_file.write('Paragraph Analysis')
+    out_file.write('\n')
+    out_file.write('-------------------')
+    out_file.write('\n')
+    out_file.write(f'Approximate Word Count:{words}')
+    out_file.write('\n')
+    out_file.write(f'Approximate Sentence Count: {sentences}')
+    out_file.write('\n')
+    out_file.write(f'Average Letter Count: {len([c for c in txt_reader if c.isalpha()])/words:.2f}')
+    out_file.write('\n')
+    out_file.write(f'Average Sentence Length: {(words/sentences):.2f}')
